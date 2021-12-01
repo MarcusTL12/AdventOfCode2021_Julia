ae9f9b9f7e0520fc56a8625437d64a64dd8b54a4
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-12-01 06:11:03 +0100

    Day1

--- src/day1.jl
+++ src/day1.jl
@@ -1,8 +1,37 @@
 
 function part1()
+    s = open("input/day1/input") do io
+        [parse(Int, l) for l in eachline(io)]
+    end
 
+    c = 0
+
+    for i = 1:length(s)-1
+        if s[i+1] > s[i]
+            c += 1
+        end
+    end
+    c
 end
 
 function part2()
+    s = open("input/day1/input") do io
+        [parse(Int, l) for l in eachline(io)]
+    end
 
+    c = 0
+
+    sums = zeros(Int, length(s) - 2)
+
+    for i in eachindex(sums)
+        sums[i] = s[i] + s[i + 1] + s[i + 2]
+    end
+
+    for i = 1:length(sums)-1
+        if sums[i+1] > sums[i]
+            c += 1
+        end
+    end
+
+    c
 end
