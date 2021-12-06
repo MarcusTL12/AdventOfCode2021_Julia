cb0e20e628118d60e4f1bfa986b4d453694fcf56
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-12-06 06:38:01 +0100

    Day 6 viz

--- src/day6.jl
+++ src/day6.jl
@@ -1,5 +1,6 @@
 using OffsetArrays
 using Plots
+using Polynomials
 
 function part1()
     inp = parse.(Int, split(first(eachline("input/day6/input")), ','))
@@ -45,6 +46,34 @@ function part2()
     sum(fish)
 end
 
-function vizualize()
-    
+function vizualize(days)
+    inp = parse.(Int, split(first(eachline("input/day6/input")), ','))
+
+    fish = OffsetArray(zeros(BigInt, 9), 0:8)
+
+    for t in inp
+        fish[t] += 1
+    end
+
+    fishes = [sum(fish)]
+
+    for _ in 1:days
+        amt_new = fish[0]
+        for i in 1:8
+            fish[i - 1] = fish[i]
+        end
+
+        fish[6] += amt_new
+        fish[8] = amt_new
+
+        push!(fishes, sum(fish))
+    end
+
+    log_fish = log10.(fishes)
+
+    p = fit(0:days, log_fish, 1)
+
+    display(p)
+
+    plot(0:days, fishes; leg=false, yscale=:log10)
 end
