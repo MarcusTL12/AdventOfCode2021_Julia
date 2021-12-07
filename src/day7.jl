72b228980bd401413ddb84cd7a0296b8a172305a
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-12-07 06:51:53 +0100

    Median solution

--- src/day7.jl
+++ src/day7.jl
@@ -1,8 +1,17 @@
+using Statistics
 
 function part1()
     inp = parse.(Int, split(readline("input/day7/input"), ','))
 
     minimum(sum(abs(x - i) for x in inp) for i = minimum(inp):maximum(inp))
+end
+
+function part1_median()
+    inp = parse.(Int, split(readline("input/day7/input"), ','))
+
+    mid = round(Int, (median!(inp)))
+
+    sum(abs(x - mid) for x in inp)
 end
 
 trig_num(x) = (x * (x + 1)) ÷ 2
