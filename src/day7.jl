2143e77ecfdbd83d033ce7dc647cfb4c3f3767ba
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-12-07 06:06:21 +0100

    Clean up

--- src/day7.jl
+++ src/day7.jl
@@ -2,9 +2,7 @@
 function part1()
     inp = parse.(Int, split(first(eachline("input/day7/input")), ','))
 
-    itr = minimum(inp):maximum(inp)
-
-    minimum(sum(abs(x - i) for x in inp) for i in itr)
+    minimum(sum(abs(x - i) for x in inp) for i = minimum(inp):maximum(inp))
 end
 
 trig_num(x) = (x * (x + 1)) ÷ 2
@@ -12,7 +10,6 @@ trig_num(x) = (x * (x + 1)) ÷ 2
 function part2()
     inp = parse.(Int, split(first(eachline("input/day7/input")), ','))
 
-    itr = minimum(inp):maximum(inp)
-
-    minimum(sum(trig_num(abs(x - i)) for x in inp) for i in itr)
+    minimum(sum(trig_num(abs(x - i)) for x in inp)
+            for i = minimum(inp):maximum(inp))
 end
