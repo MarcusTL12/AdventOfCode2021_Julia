75692ab835c936fa44b31dc2f6adf38c360b8432
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-12-12 10:34:49 +0100

    Cleanup

--- src/day7.jl
+++ src/day7.jl
@@ -34,25 +34,6 @@ function part2_mean()
         sum(trig_num(abs(x - l)) for x in inp))
 end
 
-function part2_analytic()
-    inp = parse.(Int, split(readline("input/day7/input"), ','))
-
-    x1 = 0
-    x2 = 500
-    x3 = 1000
-
-    y1 = sum(trig_num(abs(x - x1)) for x in inp)
-    y2 = sum(trig_num(abs(x - x2)) for x in inp)
-    y3 = sum(trig_num(abs(x - x3)) for x in inp)
-
-    z = (
-        x1^2 * y2 - x1^2 * y3 - x2^2 * y1 + x2^2 * y3 + x3^2 * y1 - x3^2 * y2
-    ) / (2 * (x1 * y2 - x1 * y3 - x2 * y1 + x2 * y3 + x3 * y1 - x3 * y2))
-    z = round(Int, z)
-
-    sum(trig_num(abs(x - z)) for x in inp)
-end
-
 function part2_binary()
     inp = parse.(Int, split(readline("input/day7/input"), ','))
 
