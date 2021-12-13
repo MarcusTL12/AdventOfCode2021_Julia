39663a2b7a7a1e7fa94927864843150e7584d047
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-12-13 06:34:11 +0100

    Much cleanup

--- src/day13.jl
+++ src/day13.jl
@@ -1,63 +1,58 @@
 
-function part1()
-    inp = String(open(read, "input/day13/input"))
-    first, second = split(inp, "\n\n")
+function parse_input(filename)
+    coords = Vector{Int}[]
+    folds = Tuple{Int,Int}[]
 
-    coords = [parse.(Int, split(l, ',')) for l in split(first)]
-    folds = [
-        begin
-            _, _, s = split(l)
-            axis, n = split(s, '=')
-            (axis, parse(Int, n))
-        end for l in split(second, '\n')
-    ]
+    lines = Iterators.Stateful(eachline(filename))
 
-    for (axis, n) in folds
-        cn = axis == "x" ? 1 : 2
-        for i = 1:length(coords)
-            if coords[i][cn] > n
-                coords[i][cn] = 2n - coords[i][cn]
-            end
+    for l in lines
+        if isempty(l)
+            break
         end
-        break
+        s = split(l, ',')
+        push!(coords, parse.(Int, s))
+    end
+
+    for l in lines
+        _, _, s = split(l)
+        axis, n = split(s, '=')
+        push!(folds, (axis == "x" ? 1 : 2, parse(Int, n)))
+    end
+
+    coords, folds
+end
+
+function part1()
+    coords, folds = parse_input("input/day13/input")
+
+    (cn, n) = folds[1]
+    for i = 1:length(coords)
+        if coords[i][cn] > n
+            coords[i][cn] = 2n - coords[i][cn]
+        end
     end
 
     length(Set(coords))
 end
 
 function show_grid(coords)
-    maxx = maximum(c[1] for c in coords)
-    maxy = maximum(c[2] for c in coords)
+    s = Set(coords)
 
-    grid = falses(maxy + 1, maxx + 1)
+    maxx = maximum(c[1] for c in s)
+    maxy = maximum(c[2] for c in s)
 
-    for (x, y) in coords
-        grid[y+1, x+1] = true
-    end
-
-    for row in eachrow(grid)
-        for b in row
-            print(b ? '#' : ' ')
+    for i = 0:maxy
+        for j = 0:maxx
+            print([j, i] in s ? '#' : ' ')
         end
         println()
     end
 end
 
 function part2()
-    inp = String(open(read, "input/day13/input"))
-    first, second = split(inp, "\n\n")
+    coords, folds = parse_input("input/day13/input")
 
-    coords = [parse.(Int, split(l, ',')) for l in split(first)]
-    folds = [
-        begin
-            _, _, s = split(l)
-            axis, n = split(s, '=')
-            (axis, parse(Int, n))
-        end for l in split(second, '\n')
-    ]
-
-    for (axis, n) in folds
-        cn = axis == "x" ? 1 : 2
+    for (cn, n) in folds
         for i = 1:length(coords)
             if coords[i][cn] > n
                 coords[i][cn] = 2n - coords[i][cn]
