023f13eda4c41a66b4bff8daf96fd276cb0c1dcf
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-12-14 07:10:16 +0100

    Added matrix solution

--- src/day14.jl
+++ src/day14.jl
@@ -84,3 +84,51 @@ function part2()
 
     maximum(values(char_counters)) - minimum(values(char_counters))
 end
+
+function do_step_matrix(insertions, counters)
+    new_counters = zeros(Int, 26, 26)
+
+    for j = 1:26, i = 1:26
+        if insertions[i, j] != 0
+            c = insertions[i, j]
+            new_counters[i, c] += counters[i, j]
+            new_counters[c, j] += counters[i, j]
+        else
+            new_counters[i, j] = counters[i, j]
+        end
+    end
+
+    new_counters
+end
+
+function part2_matrix()
+    lines = Iterators.Stateful(eachline("input/day14/input"))
+
+    firstline = popfirst!(lines)
+    popfirst!(lines)
+
+    insertions = zeros(Int, 26, 26)
+
+    for l in lines
+        (a1, a2), (b,) = split(l, " -> ")
+
+        insertions[a1-'A'+1, a2-'A'+1] = b - 'A' + 1
+    end
+
+    counters = zeros(Int, 26, 26)
+
+    for k in zip(firstline, Iterators.Drop(firstline, 1))
+        i, j = k .- ('A' - 1)
+        counters[i, j] += 1
+    end
+
+    for _ = 1:40
+        counters = do_step_matrix(insertions, counters)
+    end
+
+    char_counters = map(sum, eachrow(counters))
+
+    char_counters[last(firstline)-'A'+1] += 1
+
+    maximum(char_counters) - minimum(Iterators.filter(!iszero, char_counters))
+end
