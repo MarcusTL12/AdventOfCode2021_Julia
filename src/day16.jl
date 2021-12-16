2450a8b4894cea51c189c8534bd5d5b8b610e1a0
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-12-16 08:12:09 +0100

    Cleanup

--- src/day16.jl
+++ src/day16.jl
@@ -1,15 +1,15 @@
 
-function parse_packet(biterator, amt, bits_or_packets, len)
+function parse_packet(biterator, amt, bits_or_packets, len, vernumsum)
     amt_bits = 0
     amt_packets = 0
-
-    vernumsum = 0
 
     function takebit()
         amt_bits += 1
         len[] -= 1
         popfirst!(biterator)
     end
+
+    packets = Int[]
 
     while (!bits_or_packets && amt_bits < amt ||
            bits_or_packets && amt_packets < amt) && len[] > 6
@@ -18,16 +18,15 @@ function parse_packet(biterator, amt, bits_or_packets, len)
         for _ = 1:3
             push!(v, takebit())
         end
-        vnum = parse(Int, String(v); base = 2)
-        vernumsum += vnum
+        vernumsum[] += parse(Int, String(v); base = 2)
 
         t = Char[]
         for _ = 1:3
             push!(t, takebit())
         end
-        t = String(t)
+        t = parse(Int, String(t); base = 2)
 
-        if t == "100"
+        if t == 4
             bits = Char[]
             keep_on = true
             while keep_on
@@ -36,30 +35,46 @@ function parse_packet(biterator, amt, bits_or_packets, len)
                     push!(bits, takebit())
                 end
             end
-            n = parse(Int, String(bits); base = 2)
+            push!(packets, parse(Int, String(bits); base = 2))
         else
-            if takebit() == '0'
+            subpackets = if takebit() == '0'
                 num = Char[]
                 for _ = 1:15
                     push!(num, takebit())
                 end
                 n = parse(Int, String(num); base = 2)
-                (a, b) = parse_packet(biterator, n, false, len)
-                vernumsum += a
+                (a, b) = parse_packet(biterator, n, false, len, vernumsum)
                 amt_bits += b
+                a
             else
                 num = Char[]
                 for _ = 1:11
                     push!(num, takebit())
                 end
                 n = parse(Int, String(num); base = 2)
-                (a, b) = parse_packet(biterator, n, true, len)
-                vernumsum += a
+                (a, b) = parse_packet(biterator, n, true, len, vernumsum)
                 amt_bits += b
+                a
+            end
+
+            if t == 0
+                push!(packets, sum(subpackets))
+            elseif t == 1
+                push!(packets, prod(subpackets))
+            elseif t == 2
+                push!(packets, minimum(subpackets))
+            elseif t == 3
+                push!(packets, maximum(subpackets))
+            elseif t == 5
+                push!(packets, subpackets[1] > subpackets[2] ? 1 : 0)
+            elseif t == 6
+                push!(packets, subpackets[1] < subpackets[2] ? 1 : 0)
+            elseif t == 7
+                push!(packets, subpackets[1] == subpackets[2] ? 1 : 0)
             end
         end
     end
-    vernumsum, amt_bits
+    packets, amt_bits
 end
 
 function part1()
@@ -87,84 +102,12 @@ function part1()
     biterator = Iterators.Stateful(
         Iterators.flatten(translation[c] for c in inp))
 
-    parse_packet(biterator, 4 * length(inp), false, Ref(4 * length(inp)))
-end
+    vernumsum = Ref(0)
 
-function parse_packet2(biterator, amt, bits_or_packets, len)
-    amt_bits = 0
-    amt_packets = 0
+    parse_packet(
+        biterator, 4 * length(inp), false, Ref(4 * length(inp)), vernumsum)
 
-    function takebit()
-        amt_bits += 1
-        len[] -= 1
-        popfirst!(biterator)
-    end
-
-    packets = Int[]
-
-    while (!bits_or_packets && amt_bits < amt ||
-           bits_or_packets && amt_packets < amt) && len[] > 6
-        amt_packets += 1
-        v = Char[]
-        for _ = 1:3
-            push!(v, takebit())
-        end
-
-        t = Char[]
-        for _ = 1:3
-            push!(t, takebit())
-        end
-        t = parse(Int, String(t); base = 2)
-
-        if t == 4
-            bits = Char[]
-            keep_on = true
-            while keep_on
-                keep_on = takebit() == '1'
-                for _ = 1:4
-                    push!(bits, takebit())
-                end
-            end
-            push!(packets, parse(Int, String(bits); base = 2))
-        else
-            subpackets = if takebit() == '0'
-                num = Char[]
-                for _ = 1:15
-                    push!(num, takebit())
-                end
-                n = parse(Int, String(num); base = 2)
-                (a, b) = parse_packet2(biterator, n, false, len)
-                amt_bits += b
-                a
-            else
-                num = Char[]
-                for _ = 1:11
-                    push!(num, takebit())
-                end
-                n = parse(Int, String(num); base = 2)
-                (a, b) = parse_packet2(biterator, n, true, len)
-                amt_bits += b
-                a
-            end
-
-            if t == 0
-                push!(packets, sum(subpackets))
-            elseif t == 1
-                push!(packets, prod(subpackets))
-            elseif t == 2
-                push!(packets, minimum(subpackets))
-            elseif t == 3
-                push!(packets, maximum(subpackets))
-            elseif t == 5
-                push!(packets, subpackets[1] > subpackets[2] ? 1 : 0)
-            elseif t == 6
-                push!(packets, subpackets[1] < subpackets[2] ? 1 : 0)
-            elseif t == 7
-                push!(packets, subpackets[1] == subpackets[2] ? 1 : 0)
-            end
-        end
-    end
-    packets, amt_bits
+    vernumsum[]
 end
 
 function part2()
@@ -192,5 +135,6 @@ function part2()
     biterator = Iterators.Stateful(
         Iterators.flatten(translation[c] for c in inp))
 
-    parse_packet2(biterator, 4 * length(inp), false, Ref(4 * length(inp)))
+    parse_packet(
+        biterator, 4 * length(inp), false, Ref(4 * length(inp)), Ref(0))[1][1]
 end
